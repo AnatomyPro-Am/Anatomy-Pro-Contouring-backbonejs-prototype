5dcb09a3040ea42bd00d71762c246c39fa27dfff
David van Leeuwen <davz0rs@gmail.com>
2011-02-22 15:38:03 -0600

    added (local) drawing functionality

--- public/javascripts/views/AppView.js
+++ public/javascripts/views/AppView.js
@@ -1,13 +1,44 @@
 AppView = Backbone.View.extend({
 	el: $("body"),
 	initialize: function () {
-		this.friends = new Friends( null, { view: this });
+		this.user = new User( null, { view: this });
+		
+		this.scan = new Scan( null, { view: this });
+		
+		// setup websocket
 		this.socket = new io.Socket('localhost', { port: 3000 });
 		this.socket.connect();
+		
+		// setup drawing canvas
+		this.canvas = $('#drawing').dom[0];
+		this.context = this.canvas.getContext("2d");
+		this.color = {
+			r: this.randomNumber(255),
+			g: this.randomNumber(255),
+			b: this.randomNumber(255)
+		};
 	},
 	events: {
-		"click #add-friend": "addFriend",
-		"click #get-friend": "getFriend"
+		"mousedown #drawing": "startLine",
+		"mousemove #drawing": "drawLine",
+		"mouseup #drawing": "endLine"
+	},
+	startLine: function(event) {
+		this.isDrawing = true;
+		this.context.beginPath();
+		this.context.strokeStyle = "rgba("+this.color.r+","+this.color.g+","+this.color.b+",1)";
+		this.context.lineJoin = "round";
+		this.context.lineWidth = 6;
+		this.context.moveTo(event.clientX-this.canvas.offsetLeft, event.clientY-this.canvas.offsetTop);
+	},
+	drawLine: function(event) {
+		if(this.isDrawing) {
+			this.context.lineTo(event.clientX-this.canvas.offsetLeft, event.clientY-this.canvas.offsetTop);
+			this.context.stroke();
+		}
+	},
+	endLine: function() {
+		this.isDrawing = false;
 	},
 	addFriend: function () {
 		var friend = new Friend({
@@ -19,16 +50,7 @@ AppView = Backbone.View.extend({
 			data: friend
 		});
 	},
-	getFriend: function() {
-		this.socket.send({
-			event: 'getFriend'
-		});
-	},
-	addFriendToView: function (model) {
-		$("#friends-list").append(ich.friend(model.toJSON()));
-	},
-	showError: function(model, error) {
-		// not working
-		console.log(error);
+	randomNumber: function(num) {
+		return Math.floor(Math.random() * num);
 	}
 });
